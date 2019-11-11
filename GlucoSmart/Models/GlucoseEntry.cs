ae1810c3d74e1e65e185a19b5877f9af1eaf1090
Corey Stewart <stewartcj@etsu.edu>
2019-11-11 16:20:16 -0500

    Entry files added

---
+++ GlucoSmart/Models/GlucoseEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlucoSmart.Models
+{
+    public class GlucoseEntry
+    {
+        public enum ReadingType
+        {
+            PreMeal, PostMeal
+        }
+
+        public enum MealType
+        {
+            Breakfast, Lunch, Dinner
+        }
+
+        [Required]
+        public int BloodSugar { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public ReadingType Reading { get; set; }
+
+        [Required]
+        public MealType Meal { get; set; }
+    }
+}
