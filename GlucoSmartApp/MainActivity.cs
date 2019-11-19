3e24e796698546c2c1148d4ee22905f635b7168f
Corey Stewart <stewartcj@etsu.edu>
2019-11-18 20:04:16 -0500

    Mobile app added, login page works, no navigation, register in progress

---
+++ GlucoSmartApp/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using GlucoSmart.Controllers;
+using GlucoSmart.Models;
+
+namespace GlucoSmart
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        LoginController loginController;
+        ApplicationUser user;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            EditText username = FindViewById<EditText>(Resource.Id.username);
+            EditText password = FindViewById<EditText>(Resource.Id.password);
+            Button loginButton = FindViewById<Button>(Resource.Id.loginButton);
+            Button registerButton = FindViewById<Button>(Resource.Id.registerButton);
+            Button forgotButton = FindViewById<Button>(Resource.Id.forgotButton);
+
+            loginButton.Click += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(username.Text) || string.IsNullOrWhiteSpace(password.Text))
+                {
+                    Toast.MakeText(Application.Context, "User not found. Please enter valid username and password.", ToastLength.Long).Show();
+                }
+                else
+                {
+                    loginController = new LoginController(username.Text, password.Text);
+                    user = loginController.User;
+                }
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ GlucoSmartApp/RegisterActivity.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GlucoSmart.Controllers;
+using GlucoSmart.Models;
+
+namespace GlucoSmart
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+        RegisterModel register = new RegisterModel();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.register_activity);
+
+            Button clearButton = FindViewById<Button>(Resource.Id.clearButton);
+            Button registerButton = FindViewById<Button>(Resource.Id.registerButton);
+            EditText userName = FindViewById<EditText>(Resource.Id.username);
+            EditText email = FindViewById<EditText>(Resource.Id.email);
+            EditText password = FindViewById<EditText>(Resource.Id.password);
+            EditText confirmPassword = FindViewById<EditText>(Resource.Id.confirmPassword);
+            EditText firstName = FindViewById<EditText>(Resource.Id.firstName);
+            EditText lastName = FindViewById<EditText>(Resource.Id.lastName);
+            EditText dob = FindViewById<EditText>(Resource.Id.dob);
+            //EditText weight = FindViewById<EditText>(Resource.Id.weight);
+            EditText doctorID = FindViewById<EditText>(Resource.Id.DoctorID);
+
+            RegisterController registerController;
+
+            registerButton.Click
+
+
+        }
+
+        private void ClearAll()
+        {
+            EditText userName = FindViewById<EditText>(Resource.Id.username);
+            EditText email = FindViewById<EditText>(Resource.Id.email);
+            EditText password = FindViewById<EditText>(Resource.Id.password);
+            EditText confirmPassword = FindViewById<EditText>(Resource.Id.confirmPassword);
+            EditText firstName = FindViewById<EditText>(Resource.Id.firstName);
+            EditText lastName = FindViewById<EditText>(Resource.Id.lastName);
+            EditText dob = FindViewById<EditText>(Resource.Id.dob);
+            //EditText weight = FindViewById<EditText>(Resource.Id.weight);
+            EditText doctorID = FindViewById<EditText>(Resource.Id.DoctorID);
+
+
+            userName.Text = "";
+            email.Text = "";
+            password.Text = "";
+            confirmPassword.Text = "";
+            firstName.Text = "";
+            lastName.Text = "";
+            dob.Text = "";
+            //weight.Text = "";
+            doctorID.Text = "";
+        }
+    }
+}
