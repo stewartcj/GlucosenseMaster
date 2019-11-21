c211bdc5b855ebad8cc258bbe1a9fec2f1af389a
JukenRukhan <55260312+JukenRukhan@users.noreply.github.com>
2019-11-21 18:41:13 -0500

    Food Entry Model and Layout done
    @jukenrukhan

---
+++ GlucoSmartApp/Models/FoodEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GlucoSmart.Models
+{
+    class FoodEntry
+    {
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+
+        public int Carbs { get; set; }
+
+        public int Calories { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
